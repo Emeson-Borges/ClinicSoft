6c7c33afd69daee4be84a1dec8e8aae0eff89de2
Emeson Borges <emeson.itarget@gmail.com>
2024-01-03 14:30:13 -0300

    Migrações; Choices; Rotas de List

--- medicos_app/models.py
+++ medicos_app/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 class Medicos(models.Model):
+        
+    esp_choice = [
+    ('cardiologia', 'Cardiologia'),
+    ('ortopedia', 'Ortopedia'),
+    ('Ginecologista', 'Ginecologista'),
+    ('Cardiologista', 'Cardiologista')
+]
+    
     nome = models.CharField(max_length=255)
     crm = models.CharField(max_length=20, unique=True)
     telefone = models.CharField(max_length=15)
     cbo = models.CharField(max_length=10)
-    especializacao = models.CharField(max_length=255)
+    especializacao = models.CharField(max_length=255, choices=esp_choice)
     setor_id = models.IntegerField()
 
     class Meta:
--- medicos_app/urls.py
+++ medicos_app/urls.py
@@ -5,5 +5,6 @@ app_name = 'medicos_app'
 
 urlpatterns = [
     path('medicos/', viewsets.MedicosViewSet.as_view({'get': 'list'}), name='medicos-list'),
+   path('medicos/<int:pk>/', viewsets.MedicosViewSet.as_view({'get': 'retrieve'}), name='medico-detail'),
     path('medicos-create/', viewsets.MedicosViewSet.as_view({'get': 'list', 'post': 'create'}), name='medicos-list-create'),
 ]
