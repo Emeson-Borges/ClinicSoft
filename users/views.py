b65793c6077c70b34cab54e30ce78537613691d4
Emeson Borges <emeson.itarget@gmail.com>
2024-01-05 11:07:23 -0300

    Rota para criar Users

--- users/views.py
+++ users/views.py
@@ -6,49 +6,11 @@ from rest_framework import status
 from .serializers import UserSerializer
 from rest_framework.decorators import action
 from rest_framework_simplejwt.tokens import RefreshToken
+from .models import CustomUser
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-
-    # @action(detail=False, methods=['post'])
-    # def login(self, request):
-    #     username = request.data.get('cpf')  # Login usando username=cpf
-    #     password = request.data.get('password')
-
-    #     user = authenticate(request, username=username, password=password)
-
-    #     if user is not None:
-    #         refresh = RefreshToken.for_user(user)
-    #         access_token = str(refresh.access_token)
-    #         token_expires = refresh.access_token.payload['exp']
-
-    #         return Response({
-    #             'user_info': {
-    #                 'id': user.id,
-    #                 'name': user.username,
-    #                 'email': user.email,
-    #                 'token_expires': token_expires,
-    #             },
-    #             'token': access_token,
-    #         })
-    #     else:
-    #         return Response({'non_field_errors': ['Impossível fazer login com as credenciais fornecidas.']}, status=400)
-
-    # @action(detail=False, methods=['post'])
-    # def user_create(self, request):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_create(serializer)
-
-    #     # Adicione o campo de senha ao retorno
-    #     response_data = serializer.data
-    #     response_data['password'] = '********'  # Adapte conforme necessário
-
-    #     headers = self.get_success_headers(serializer.data)
-
-    #     return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
-
 
     @action(detail=False, methods=['post'])
     def login(self, request):
@@ -77,10 +39,28 @@ class UserViewSet(viewsets.ModelViewSet):
 
         return Response({'non_field_errors': ['Impossível fazer login com as credenciais fornecidas.']}, status=400)
     
-    
     @action(detail=False, methods=['get'])
     def user_list(self, request):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
 
         return Response(serializer.data)
+    
+    def user_create(self, request, *args, **kwargs):
+        if request.method == 'GET':
+            # Handle GET requests
+            # For example, you might want to retrieve a list of users
+            users = CustomUser.objects.all()
+            serializer = CustomUser(users, many=True)
+            return Response(serializer.data)
+        elif request.method == 'POST':
+            # Handle POST requests
+            # For example, you might want to create a new user
+            serializer = CustomUser(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=201)
+            return Response(serializer.errors, status=400)
+        else:
+            # Handle other HTTP methods if needed
+            return Response({'error': 'Method not allowed'}, status=405)
