9282c0e07f68f6d4d4529e2493d3be532f88d76e
Emeson Borges <emeson.itarget@gmail.com>
2024-01-05 11:18:09 -0300

    Rota para criar Users

--- users/views.py
+++ users/views.py
@@ -14,13 +14,13 @@ class UserViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['post'])
     def login(self, request):
-        cpf = request.data.get('cpf')
+        username = request.data.get('username')
         password = request.data.get('password')
 
-        if cpf is None or password is None:
-            return Response({'error': 'Por favor, forneça CPF e senha.'}, status=400)
+        if username is None or password is None:
+            return Response({'error': 'Por favor, forneça CPF e senha.'}, status=status.HTTP_400_BAD_REQUEST)
 
-        user = authenticate(request, cpf=cpf, password=password)
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
             refresh = RefreshToken.for_user(user)
@@ -37,8 +37,8 @@ class UserViewSet(viewsets.ModelViewSet):
                 'token': access_token,
             })
 
-        return Response({'non_field_errors': ['Impossível fazer login com as credenciais fornecidas.']}, status=400)
-    
+        return Response({'non_field_errors': ['Impossível fazer login com as credenciais fornecidas.']}, status=status.HTTP_400_BAD_REQUEST)
+   
     @action(detail=False, methods=['get'])
     def user_list(self, request):
         queryset = self.get_queryset()
